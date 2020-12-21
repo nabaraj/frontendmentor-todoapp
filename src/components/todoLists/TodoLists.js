d7b5eedfc563a88d3b247b703aeffd26859efd55
Nabaraj Saha <nabara2k@gmail.com>
2020-12-21 13:11:57 +0530

    update draggable

--- src/components/todoLists/TodoLists.js
+++ src/components/todoLists/TodoLists.js
@@ -1,74 +1,76 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import CustomCheckBox from '../customCheckbox/CustomCheckBox';
-import {sortableContainer, sortableElement} from 'react-sortable-hoc';
+import { sortableContainer, sortableElement } from 'react-sortable-hoc';
 import cross from './../../images/icon-cross.svg';
 
-
-const SortableItem = sortableElement(({item, checkEvent, itemIndex, removeItem})=>{
-  const changeEvent = function(e){
+function ListItem({ item, checkEvent, itemIndex, removeItem }) {
+  const changeEvent = function (e) {
     let checked = e.target.checked;
     let currentItem = item;
-    if(checked){
+    if (checked) {
       currentItem.status = 'completed'
-    }else{
+    } else {
       currentItem.status = 'active'
     }
     checkEvent(currentItem);
   }
   return (<li className="list-group-item d-flex align-items-center pl-4">
-          <CustomCheckBox 
-            name={`${itemIndex}-todoList`}
-            labelText = {item.content}
-            checked = {item.status === 'completed'}
-            checkEvent = {changeEvent}
-          />
-          <div className="ml-auto" onClick={(e)=>removeItem(itemIndex)}>
-            <img src={cross} className="pointer" alt=""/>
-          </div>
-        </li>)
-})
-const SortableContainer = sortableContainer(({children}) => {
+    <CustomCheckBox
+      name={`${itemIndex}-todoList`}
+      labelText={item.content}
+      checked={item.status === 'completed'}
+      checkEvent={changeEvent}
+    />
+    <div className="ml-auto" onClick={(e) => removeItem(itemIndex)}>
+      <img src={cross} className="pointer" alt="" />
+    </div>
+  </li>)
+}
+const SortableItem = sortableElement(ListItem);
+const SortableContainer = sortableContainer(({ children }) => {
   return <ul className="todoListUl list-group">{children}</ul>;
 });
 
-const TodoLists = ({filteredList, todoLists, actionTodo, removeItem, filterList, statusArray, activeFilter, clearCompleted, onSortEnd}) => {
-  
-  const countComplete = ()=>{
-    let activeCount = todoLists.filter(item=>item.status==='active').length;
-    return activeCount > 1 ? `${activeCount} items left`:`${activeCount} item left`
+const TodoLists = ({ filteredList, todoLists, actionTodo, removeItem, filterList, statusArray, activeFilter, clearCompleted, onSortEnd }) => {
+
+  const countComplete = () => {
+    let activeCount = todoLists.filter(item => item.status === 'active').length;
+    return activeCount > 1 ? `${activeCount} items left` : `${activeCount} item left`
   }
-  const changeFilter = (type)=>{
+  const changeFilter = (type) => {
     filterList(type);
   }
   return (
-    <SortableContainer onSortEnd={onSortEnd}>
-      {filteredList.map((item,index)=>(
-        <SortableItem 
-          item={item} 
-          itemIndex={index} 
+    <SortableContainer onSortEnd={onSortEnd} 
+   
+    distance={1}>
+      {filteredList.map((item, index) => (
+        <SortableItem
+          item={item}
+          itemIndex={index}
           checkEvent={actionTodo}
           key={`${item.content}-${index}`}
-          removeItem = {removeItem}
-          index={index} 
+          removeItem={removeItem}
+          index={index}
         />
-        ))
+      ))
       }
       <li className="list-group-item d-flex justify-content-between">
         <div>{countComplete()}</div>
         <ul className="list-inline d-flex justify-content-between">
-          {statusArray.map((item)=>(
-            <li 
-              onClick={(e)=>changeFilter(item)}
-              className={`px-1 text-capitalize footerList ${activeFilter===item?'active':''}`} 
+          {statusArray.map((item) => (
+            <li
+              onClick={(e) => changeFilter(item)}
+              className={`px-1 text-capitalize footerList ${activeFilter === item ? 'active' : ''}`}
               key={item}>
               {item}
             </li>))}
         </ul>
-        <div 
+        <div
           onClick={clearCompleted}
           className="footerList">Clear Completed</div>
       </li>
-      </SortableContainer>
+    </SortableContainer>
   );
 }
 
